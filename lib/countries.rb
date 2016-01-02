0c33969ac558ac86d22f89f91f72c9d64a3f4dc9
Vinh Nguyen <vinh.nglx@gmail.com>
2016-01-02 11:18:28 +0700

    Writes test cases - context 'attributes' - for ipaddress model

---
+++ lib/countries.rb
@@ -0,0 +1,40 @@
+class Countries
+  # Public: Initialize a new countries object
+  #
+  # csv_file  - The path to current country csv file
+  #
+  # Examples
+  #
+  #   Countries.new(/path/to/csv_file)
+  #
+  # Returns the object
+  def initialize(csv_file)
+
+  end
+
+  # Public: Show array of country codes
+  #
+  # Examples
+  #
+  #   countries = Countries.new(/path/to/csv/file)
+  #   countries.country_codes
+  #   # => ['AU', 'VN', 'US']
+  #
+  # Returns the array
+  def country_codes
+
+  end
+
+  # Public: Show array of country names
+  #
+  # Examples
+  #
+  #   countries = Countries.new(/path/to/csv/file)
+  #   countries.country_names
+  #   # => ['Australia', 'Viet Nam', 'United States']
+  #
+  # Returns the array
+  def country_names
+
+  end
+end
--- spec/models/ipaddress_spec.rb
+++ spec/models/ipaddress_spec.rb
@@ -13,5 +13,32 @@
 require 'rails_helper'
 
 RSpec.describe Ipaddress, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  context 'attributes' do
+    it 'has ip_addresses' do
+      expect(build(:ipaddress, ip_addresses: '{option_1: [16843008, 16843263], option_2: [16777216, 16777471]}')).
+        to have_attributes(ip_addresses: '{option_1: [16843008, 16843263], option_2: [16777216, 16777471]}')
+    end
+
+    it 'has country_code' do
+      expect(build(:ipaddress, country_code: 'AU')).to have_attributes(country_code: 'AU')
+    end
+
+    it 'has country_name' do
+      expect(build(:ipaddress, country_name: 'Australia')).to have_attributes(country_name: 'Australia')
+    end
+  end
+
+  context 'validations' do
+    let(:country) { Countries.new(Rails.root.join('lib', 'countries', 'countries.csv').to_path) }
+    let(:country_codes) { country.country_codes }
+    let(:country_names) { country.country_names }
+
+    it do
+      should validate_inclusion_of(:country_code).in_array(country_codes)
+    end
+
+    it do
+      should validate_inclusion_of(:country_name).in_array(country_names)
+    end
+  end
 end
